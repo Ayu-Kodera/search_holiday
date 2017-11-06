2c0f9d66905195b706ce8676937e345ce57a2bc9
ayu.kodera@gaiax.com <ayu.kodera@GX1946.local>
2017-11-06 19:43:58 +0900

    クリックした日付が休日かどうかの判定機能完了！

--- app/assets/javascripts/calendar.coffee
+++ app/assets/javascripts/calendar.coffee
@@ -2,16 +2,26 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
 $(document).ready ->
-  $('#calendar').fullCalendar dayClick: ->
-    base_date = 3
-    input_date = 15
-    passed_date = input_date - base_date
-    seek_date = passed_date % 6
-    result = 0
-    if seek_date == 0 or seek_date == 1
-      result = input_date + '日は休日です！'
-    else
-      result = input_date + '日は出勤日です。'
-    alert result
-    return
+  $('#calendar').fullCalendar
+    events: '/events.json'
+    selectable: true
+    selectHelper: true
+    dayClick: (date) ->
+      base_date = "2017-11-03"
+      console.log(base_date)
+      #parse_date = moment(base_date).format('MM/DD/YYYY')
+      #console.log(parse_date)
+      input_date = moment(date).format('YYYY/MM/DD')
+      console.log(input_date)
+      passed_date = moment(input_date).diff(base_date, 'days')
+      console.log(passed_date)
+      seek_date = passed_date % 6
+      #console.log(seek_date)
+      result = 0
+      if seek_date == 0 or seek_date == 1
+        result = input_date + '日は休日です！'
+      else
+        result = input_date + '日は出勤日です。'
+      alert result
+      return
   return
--- app/views/calendar/pre_change.js
+++ app/views/calendar/pre_change.js
@@ -1,18 +1,23 @@
 // クリック時発火イベント
 $(document).ready(function() {
     $('#calendar').fullCalendar({
-        dayClick: function() {
+        events: '/events.json',
+        selectable: true,
+        selectHelper: true,
+        dayClick: function(date) {
           var base_date = 3;
-        	var input_date = 15;
-        	var passed_date = input_date - base_date;
-        	var seek_date = passed_date % 6;
-        	var result = 0;
+          var input_date = 15;
+          var passed_date = input_date - base_date;
+          var seek_date = passed_date % 6;
+          var result = 0;
+          var str = moment( date ).format( 'YYYY/MM/DD' );
           if (seek_date === 0 || seek_date === 1) {
             result = input_date + "日は休日です！"
           } else {
             result = input_date + "日は出勤日です。"
           };
           alert(result);
+          alert(str);
         }
     });
 });
@@ -30,3 +35,17 @@ if (seek_date == 0 || seek_date == 1) {
 } else {
   result = input_date + "日は出勤日です。"
 };
+
+
+// クリックした日付を取得
+$(function(){
+  $('#calendar').fullCalendar({
+        events: '/events.json',
+        selectable: true,
+        selectHelper: true,
+        dayClick: function(date) {
+            var str = moment( data ).format( 'YYYY/MM/DD' );
+            console.log(date);
+        }
+    });
+});
