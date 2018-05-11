7f6edda55345f38950a48d240df010358f6c5739
kaito.shibuya <kaito.shibuya@gaiax.com>
2018-05-11 16:01:33 +0900

    unko

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,16 +8,29 @@ class ApplicationController < ActionController::Base
   def which_day
     base_date = 3
     #input_date = STDIN.gets.chomp
-    input_date = 15
-    passed_date = input_date - base_date
+    input_date = 10
+    pass_date = input_date - base_date
     seek_date = passed_date % 6
 
-    if (seek_date == 0 || seek_date == 1)
+    if (seek_date == 0 && seek_date == 1)
       result = "#{input_date}日は休日です！"
     else
       result = "#{input_date}日は出勤日です。"
     end
 
+    print input_date
+    print input_date
+    print input_date
+    print input_date
+    print input_date
+    print input_date
+    print input_date
+    print input_date
+    print input_date
+    print input_date
+    print input_date
+    print input_date
+    print input_date
     render html:result
   end
 end
